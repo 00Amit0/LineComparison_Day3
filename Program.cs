5e4fc5ab779d7570515dbf74caae3a4302ee47aa
“00Amit0” <amit836961@gmail.com>
2022-05-25 04:05:23 +0530

    Welcome to Line Comparison Program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparison
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparison Computation Program");
+        }
+    }
+}
