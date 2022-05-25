e800476e4a3ef4c504c31ed0b7ca9e762670bec3
“00Amit0” <amit836961@gmail.com>
2022-05-25 08:21:51 +0530

    Program to compare two lines

---
+++ LineCompare.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class LineCompare
+    {
+        public static void LineLengths()
+        {
+            double a1 , a2 , b1 , b2 ,x1, x2, y1, y2;
+            a1 = 10d;
+            a2 = 12d;
+            b1 = 13d;
+            b2 = 11d;
+            x1 = 12d;
+            x2 = 13d;
+            y1 = 11d;
+            y2 = 10d;
+            var Line1 = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine(Line1);
+            var Line2 = Math.Sqrt((Math.Pow(a1 - a2, 2) + Math.Pow(b1 - b2, 2)));
+            Console.WriteLine(Line2);
+            if (Line1 == Line2)
+            {
+                Console.WriteLine("Lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal");
+            }
+        }
+    }
+}
